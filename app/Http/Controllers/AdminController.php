82ad0f7c0594bfbbadfa5302d07bed54f462410c
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-04-29 15:19:16 +0100

    Ajout de la gestion des utilisateurs dans le contrôleur et le dépôt admin, y compris les méthodes pour modifier et supprimer un utilisateur, ainsi que l'affichage des utilisateurs dans le tableau de bord admin.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Utilisateur;
+use App\Repositories\Interfaces\AdminRepositoryInterface;
 use App\Repositories\Interfaces\TagRepositoryInterface;
 use App\Repositories\Interfaces\ArticleRepositoryInterface;
 use App\Repositories\Interfaces\CategorieRepositoryInterface;
+use App\Repositories\Interfaces\CommandeRepositoryInterface;
+use App\Repositories\Interfaces\ProduitRepositoryInterface;
+use App\Repositories\Interfaces\RendezVousRepositoryInterface;
+use App\Repositories\Interfaces\UtilisateurRepositoryInterface;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -12,12 +18,22 @@ class AdminController extends Controller
     private $tagRepository;
     private $articleRepository;
     private $categorieRepository;
+    private $adminRepository;
+    private $utilisateurRepository;
+    private $produitRepository;
+    private $commandeRepository;
+    private $rendezVousRepository;
 
-    public function __construct(TagRepositoryInterface $tagRepository, ArticleRepositoryInterface $articleRepository, CategorieRepositoryInterface $categorieRepository)
+    public function __construct(RendezVousRepositoryInterface $rendezVousRepository, CommandeRepositoryInterface $commandeRepository, ProduitRepositoryInterface $produitRepository, AdminRepositoryInterface $adminRepository, TagRepositoryInterface $tagRepository, ArticleRepositoryInterface $articleRepository, CategorieRepositoryInterface $categorieRepository, UtilisateurRepositoryInterface $utilisateurRepository)
     {
         $this->tagRepository = $tagRepository;
         $this->articleRepository = $articleRepository;
         $this->categorieRepository = $categorieRepository;
+        $this->adminRepository = $adminRepository;
+        $this->utilisateurRepository = $utilisateurRepository;
+        $this->produitRepository = $produitRepository;
+        $this->commandeRepository = $commandeRepository;
+        $this->rendezVousRepository = $rendezVousRepository;
     }
 
     public function getAdminById(int $id)
@@ -27,7 +43,71 @@ class AdminController extends Controller
 
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $countUtilisateurs = $this->utilisateurRepository->countUsers();
+        $countProduits = $this->produitRepository->countProduit();
+        $countCommandes = $this->commandeRepository->countCommandes(); 
+        $countconsultations = $this->rendezVousRepository->countConsultations(); 
+
+        $allUtilisateurs = $this->utilisateurRepository->getAllUtilisateurs(); 
+
+        $statistiques = [
+            'utilisateurs' => $countUtilisateurs,
+            'produits' => $countProduits,
+            'commandes' => $countCommandes,
+            'consultations' => $countconsultations,
+            'allUtilisateurs' => $allUtilisateurs
+        ];
+
+        return view('admin.dashboard', compact('statistiques'));
+    }
+
+    public function usersIndex (){
+        $utilisateurs = $this->utilisateurRepository->getAllUtilisateurs();
+        // dd($utilisateurs);
+        return view('admin.users.index', compact('utilisateurs'));
+    }
+
+    public function usersShow (Request $request){
+        // dd($request->id);
+        $utilisateur = $this->utilisateurRepository->getById($request->id);
+        // dd($utilisateurs);
+        return view('admin.users.show', compact('utilisateur'));
+    }
+
+    public function usersEdit (Request $request){
+        // dd($request->id);
+        $utilisateur = $this->utilisateurRepository->getById($request->id);
+        // dd($utilisateurs);
+        return view('admin.users.edit', compact('utilisateur'));
+    }
+
+    public function usersEditSubmit(Request $request){
+        // dd($request->all());
+        $validated = $request->validate([
+            'nom' => 'required|string|max:255',
+            'prenom' => 'required|string|max:255',
+            'email' => 'required|email|unique:utilisateurs,email,',
+            'telephone' => 'required|string|max:13',
+            'adresse' => 'required|string',
+            'type' => 'required|string|in:client,veterinaire,agricole,admin',
+            'photo' => 'string'
+        ]);
+        // dd($validated);
+        $id = $request->id;
+        $modified = $this->adminRepository->modifierUtilisateur($id, $validated);
+        if ($modified) {
+            $utilisateur = $this->utilisateurRepository->getById($id);
+            return view('admin.users.show', compact('utilisateur'));
+        }
+        // dd($utilisateur); 
+    }
+
+    public function usersDelete(Request $request){
+        $validated = $request->validate([
+            'id' => 'required|integer'
+        ]);
+        $this->adminRepository->supprimerUtilisateur($validated['id']);
+        return redirect()->route('admin.users.index')->with('success', 'Utilisateur supprimé avec succès !');
     }
 
     public function getUtilisateurAdminById(int $id)
@@ -65,12 +145,16 @@ class AdminController extends Controller
             } else {
                 $article->photo = "https://placehold.co/600x400/$backgroundColor/$textColor?text=$encodedTitle";
             }
+
+            $article->auteur = $this->getAdminById($article->auteur);
+            // echo $article->auteur . "<br>";
+            // dd($article->auteur);
+            $idUtilisateur = $article->auteur->compte;    
+            $Utilisateuradmin = $this->getUtilisateurAdminById($idUtilisateur);
+            $article->auteur = $Utilisateuradmin;
         }
 
-        $admin = $this->getAdminById($articles->toArray()['data'][0]['auteur']);
-        $Utilisateuradmin = $this->getUtilisateurAdminById($articles->toArray()['data'][0]['auteur']);
-
-        return view('admin.articles.index', compact('articles', 'admin', 'Utilisateuradmin', 'categorieNom'));
+        return view('admin.articles.index', compact('articles','Utilisateuradmin', 'categorieNom'));
     }
 
     public function articlesCreate()
@@ -85,31 +169,92 @@ class AdminController extends Controller
             'titre' => 'required|string|max:255',
             'photo' => 'required|string',
             'statut' => 'required|string',
-            'contenu' => 'required|string'
+            'contenu' => 'required|string',
+            'auteur_id' => 'required|integer'
         ]);
+        // dd($validated);
 
         $categories = $this->categorieRepository->getAllCategories();
         
         return view('admin.articles.add-categorie', compact('validated', 'categories'));
     }
 
-    public function articlesStorewithCategorie(Request $request)
+    public function articlesEditStore(Request $request)
     {
+        // dd($request->all());
         $validated = $request->validate([
             'titre' => 'required|string|max:255',
             'photo' => 'required|string',
             'statut' => 'required|string',
             'contenu' => 'required|string',
-            'categorie_id' => 'required|integer|exists:categories,id',
+            'article_id' => 'required|integer',
+            'categorie_id' => 'required|integer',
             'categorie' => 'required|string'
         ]);
 
+        $categories = $this->categorieRepository->getAllCategories();
+        
+        return view('admin.articles.update-with-categorie', compact('validated', 'categories'));
+    }
+
+    public function articlesEditStoreSubmit(Request $request){
+        // dd($request->all());
+        $validated = $request->validate([
+            'titre' => 'required|string|max:255',
+            'contenu' => 'required|string',
+            'photo' => 'required|string',
+            'statut' => 'required|string',
+            'article_id' => 'required|integer',
+            'categorie_id' => 'required|integer'
+        ]);
+        // dd($validated);
+        $categorie = $this->categorieRepository->getCategorieById($validated['categorie_id']);
+        $article = $this->articleRepository->getArticleById($validated['article_id']);
+        // dd($categorie->nom);
+        $validated = array_merge($validated, ['categorie' => $categorie->nom]);
+        $validated = array_merge($validated, ['auteur' => $article->auteur]);
+        // dd($validated['article_id']);
+        $modified = $this->articleRepository->modifierArticle($validated['article_id'], $validated);
+        if ($modified) {
+            return redirect()->route('admin.articles.index');
+        }
+    }
+
+    public function articlesStorewithCategorie(Request $request)
+    {
+        // dd($request->all());
+        $validated = $request->validate([
+            'titre' => 'required|string|max:255',
+            'contenu' => 'required|string',
+            'photo' => 'required|string',
+            'statut' => 'required|string',
+            'auteur_id' => 'required|integer',
+            'categorie_id' => 'required|integer|exists:categories,id'
+        ]);
+        // dd($validated);
+        $auteur = $this->adminRepository->getByUtilisateurId($validated['auteur_id']);
+        // dd($auteur);
+        $validated['auteur_id'] = $auteur->id;
+        $categorie = $this->categorieRepository->getCategorieById($validated['categorie_id']);
+        $validated = array_merge($validated, ['categorie' => $categorie->nom]);
+        // dd($validated);
         $this->articleRepository->creeArticle($validated);
         return redirect()->route('admin.articles.index')->with('success', 'Article ajouter avec succès !');
     }
-    public function articlesUpdate()
-    {
-        return view('admin.articles.update');
+
+    public function articlesSupprimer(Request $request){
+        $validated = $request->validate([
+            'id' => 'required|integer'
+        ]);
+        // dd($validated['id']);
+        $this->articleRepository->supprimerArticle($validated['id']);
+        return redirect()->route('admin.articles.index')->with('success', 'Article supprimé avec succès !');
+    }
+
+    public function articlesEdit(Request $request){
+        // dd($request->all());
+        $article = $this->articleRepository->getArticleById($request->id);
+        return view('admin.articles.update', compact('article'));
     }
 
     public function tagsIndex()
