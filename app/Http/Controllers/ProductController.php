34df3f8094184a7385876d43b51cc48c16cc4dd0
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-02 16:02:15 +0100

    Ajout de la gestion du panier dans le contrôleur des produits et correction de la méthode getAllCommandes dans le dépôt des commandes.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\Interfaces\CategorieRepositoryInterface;
+use App\Repositories\Interfaces\CommandeRepositoryInterface;
 use App\Repositories\Interfaces\ProduitRepositoryInterface;
 use Illuminate\Http\Request;
 
@@ -10,11 +11,13 @@ class ProductController extends Controller
 {
     protected $produitRepository;
     protected $categorieRepository;
+    protected $commandeRepository;
     
-    public function __construct(ProduitRepositoryInterface $produitRepository, CategorieRepositoryInterface $categorieRepository)
+    public function __construct(CommandeRepositoryInterface $commandeRepository, ProduitRepositoryInterface $produitRepository, CategorieRepositoryInterface $categorieRepository)
     {
         $this->produitRepository = $produitRepository;
         $this->categorieRepository = $categorieRepository;
+        $this->commandeRepository = $commandeRepository;
     }
 
     public function index(){
@@ -50,5 +53,15 @@ class ProductController extends Controller
         }
         // dd($product->categorie);
         return view('products.show', compact('product'));
+    }
+
+    public function cartIndex(Request $request){
+        $cart = json_decode($request['cart'], true);
+        // dd($pp);
+        // $cart = $this->commandeRepository->getAllCommandes();
+        // $user =  $request->session()->put('cart', $request->input('cart'));
+        // dd($user);
+        // dd($cart);
+        return view('cart.index', compact('cart'));
     }
 }
