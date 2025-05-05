37257c7dd947d83a85482a6b091ff34f48ccbb97
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-05 10:49:56 +0100

    J'ai ajouter la logique de ajouter des info suplementaire pour le veterinarie

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Repositories\Interfaces\UtilisateurRepositoryInterface;
 use App\Repositories\Interfaces\AgricoleRepositoryInterface;
+use App\Repositories\Interfaces\VeterinaireRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
@@ -12,11 +13,13 @@ class AuthController extends Controller
 {
     protected $utilisateurRepository;
     protected $agricoleRepository;
+    protected $veterinaireRepository;
 
-    public function __construct(UtilisateurRepositoryInterface $utilisateurRepository, AgricoleRepositoryInterface $agricoleRepository)
+    public function __construct(VeterinaireRepositoryInterface $veterinaireRepository, UtilisateurRepositoryInterface $utilisateurRepository, AgricoleRepositoryInterface $agricoleRepository)
     {
         $this->utilisateurRepository = $utilisateurRepository;
         $this->agricoleRepository = $agricoleRepository;
+        $this->veterinaireRepository = $veterinaireRepository;
     }
 
     public function showRegistrationForm()
@@ -33,7 +36,7 @@ class AuthController extends Controller
         if (Auth::check()) {
             return redirect()->route('profile.show');
         }
-        
+
         return view('auth.login');
     }
 
@@ -57,17 +60,40 @@ class AuthController extends Controller
 
 
         $utilisateur = $this->utilisateurRepository->inscription($validated);
-        
-        if($utilisateur['type'] == 'agricole'){
+        // dd($utilisateur);
+
+        if ($utilisateur['type'] == 'agricole') {
             $utilisateurId = $utilisateur['id'];
-            $utilisateurIdToCreate = ['compte' => $utilisateurId, 
-            'ferme' => 'non spécifié',
-            'produit' => 'non spécifiée',
-            'superficie_terrain' => 'non spécifiée',
-            'region' => 'non spécifiée'
-        ];
+
+            $utilisateurIdToCreate = [
+                'compte' => $utilisateurId,
+                'ferme' => 'non spécifié',
+                'produit' => 'non spécifiée',
+                'superficie_terrain' => 'non spécifiée',
+                'region' => 'non spécifiée'
+            ];
+
             $this->agricoleRepository->create($utilisateurIdToCreate);
+
+
+        }elseif ($utilisateur['type'] == 'veterinaire') {
+            $utilisateurId = $utilisateur['id'];
+            // dd($utilisateurId);
+
+            $utilisateurIdToCreate = [
+                'specialite' => 'non spécifié',
+                'diplome' => 'non spécifiée',
+                'annee_experience' => 'non spécifiée',
+                'compte' => $utilisateurId,
+                'prix_deplacement' => 0
+            ];
+
+            // dd($utilisateurIdToCreate);
+            $this->veterinaireRepository->create($utilisateurIdToCreate);
+            // dd($rr);
         }
+
+        
 
         Auth::login($utilisateur);
 
@@ -86,7 +112,7 @@ class AuthController extends Controller
             $validated['password']
         );
 
-        
+
 
         Auth::login($utilisateur, $request->has('remember'));
 
